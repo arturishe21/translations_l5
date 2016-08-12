416aaa0b7af1247fa0ab6272f3f747349c7faa36
artur <artur@vis-design.com>
2016-08-12 16:01:43 +0300

    add function_exists

--- src/Http/helpers.php
+++ src/Http/helpers.php
@@ -2,26 +2,33 @@
 use Vis\Translations\Trans;
 
 //get translate
-function __($phrase, array $replacePhrase = []) {
-    $thisLang = Lang::locale();
-    $array_translate =  Trans::fillCacheTrans();
+if (!function_exists('__')) {
+    function __ ($phrase, array $replacePhrase = [])
+    {
+        $thisLang = Lang::locale ();
+        $array_translate = Trans::fillCacheTrans ();
 
-    if (isset($array_translate[$phrase][$thisLang])) {
-        $phrase = $array_translate[$phrase][$thisLang];
-    } else {
-        $phrase = Trans::generateTranslation($phrase, $thisLang);
+        if (isset($array_translate[$phrase][$thisLang])) {
+            $phrase = $array_translate[$phrase][$thisLang];
+        } else {
+            $phrase = Trans::generateTranslation ($phrase, $thisLang);
+        }
+
+        if (count ($replacePhrase)) {
+            $phrase = str_replace (array_keys ($replacePhrase),
+                array_values ($replacePhrase), $phrase);
+        }
+
+        return $phrase;
     }
-
-    if (count($replacePhrase)) {
-        $phrase = str_replace(array_keys($replacePhrase), array_values($replacePhrase), $phrase);
-    }
-
-    return $phrase;
 }
 
-function cmp($a, $b) {
-    if ($a == $b) {
-        return 0;
+if (!function_exists('cmp')) {
+    function cmp ($a, $b)
+    {
+        if ($a == $b) {
+            return 0;
+        }
+        return (strlen ($a) < strlen ($b)) ? -1 : 1;
     }
-    return ( strlen($a) < strlen($b)) ? -1 : 1;
 }
