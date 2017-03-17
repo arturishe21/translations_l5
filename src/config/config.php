defff0c5c4ede7f2d5dd6da0843efbd4d7e4bdb4
artur <artur@vis-design.com>
2017-03-17 16:14:03 +0100

    add language array

--- src/config/config.php
+++ src/config/config.php
@@ -7,5 +7,24 @@ return array(
     'alt_langs'=>  array ("Рус"=>"ru", "Укр"=>"ua", 'Eng'=>"en"),
     'title_page' => "Переводы",
     'show_count' => array("20","40","60","100"),
+
+    'languages' => [
+        array(
+            'caption' => 'ru',
+            'postfix' => '',
+            'placeholder' => 'Текст на русском',
+        ),
+        array(
+            'caption' => 'ua',
+            'postfix' => '_ua',
+            'placeholder' => 'Текст на украинском',
+        ),
+
+        array(
+            'caption' => 'en',
+            'postfix' => '_en',
+            'placeholder' => 'Текст на английском',
+        ),
+    ]
 );
 
