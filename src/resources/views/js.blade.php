aab071a4ed18b45c4507002fe139183d0a4d9d86
artur <artur@vis-design.com>
2018-03-02 11:14:04 +0200

    delete \n

--- src/resources/views/js.blade.php
+++ src/resources/views/js.blade.php
@@ -1,3 +1,3 @@
-var obj = {};@foreach($data as $phrase => $translate)obj['{{$phrase}}'] = '{{$translate[$lang] or ''}}';@endforeach
+var obj = {};@foreach($data as $phrase => $translate)obj['{{ str_replace(array("\r\n", "\r", "\n"), '', $phrase) }}'] = '{{isset($translate[$lang]) ?  str_replace(array("\r\n", "\r", "\n"), '', $translate[$lang]) : ''  }}';@endforeach
 window.__t = function(phrase){ if (typeof obj[phrase] != "undefined") { return obj[phrase]; } else { return ''; }}
 
